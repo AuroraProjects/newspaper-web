8ab3be260f30eab7d6b3a4f98c4d1078c7c26641
MisakaTAT <i@mikuac.com>
2022-04-05 13:15:15 +0800

    add prettierrc rule

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import prismjs from 'vite-plugin-prismjs'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,9 @@ export default defineConfig({
     vue(),
     prismjs({
       languages: 'all',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
     })
   ],
   server: {
